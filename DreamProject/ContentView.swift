2d8fab3841d45ded1381e7ea8924be8ca62d8c53
byounghoon <futurum87@gmail.com>
2019-09-27 21:34:36 +0900

    Initial Commit

---
+++ DreamProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DreamProject
+//
+//  Created by ByoungHoon Yun on 27/09/2019.
+//  Copyright © 2019 ByoungHoon. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

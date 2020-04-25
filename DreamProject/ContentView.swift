f71a055d639b2abf6f2de0b45828b9f715b9a9a3
byounghoon <futurum87@gmail.com>
2020-04-25 21:54:10 +0900

    update
    - 로또 번호 5게임으로 변경

--- DreamProject/ContentView.swift
+++ DreamProject/ContentView.swift
@@ -10,32 +10,49 @@ import SwiftUI
 
 struct ContentView: View {
     
-    var magicNumber : [Int] = [1, 4, 5, 8, 10, 11, 12, 13, 14, 15, 18, 19, 20, 21, 24, 26, 31, 33, 36, 37, 39, 45]
+    var magicNumber : [Int] = [1, 4, 5, 8, 10,
+                               11, 12, 13, 14, 15,
+                               18, 19, 20, 21, 24,
+                               26, 31, 33, 36, 37,
+                               39, 45]
     
     @State var firstNumber: String = " "
+    @State var secondNumber: String = " "
+    @State var thirdNumber: String = " "
+    @State var fourthNumber: String = " "
+    @State var fifthNumber: String = " "
     
     var body: some View {
         
         NavigationView {
             VStack(alignment: HorizontalAlignment.center, spacing: 30) {
                 Text(firstNumber).font(Font.system(size: 30, weight: Font.Weight.bold, design: Font.Design.monospaced))
+                Text(secondNumber).font(Font.system(size: 30, weight: Font.Weight.bold, design: Font.Design.monospaced))
+                Text(thirdNumber).font(Font.system(size: 30, weight: Font.Weight.bold, design: Font.Design.monospaced))
+                Text(fourthNumber).font(Font.system(size: 30, weight: Font.Weight.bold, design: Font.Design.monospaced))
+                Text(fifthNumber).font(Font.system(size: 30, weight: Font.Weight.bold, design: Font.Design.monospaced))
                 Button(action: {
-                
-                    self.firstNumber = self.magicNumber
-                        .shuffled()
-                        .shuffled()
-                        .shuffled()
-                        .enumerated()
-                        .compactMap{ $0 < 6 ? $1 : nil }
-                        .map{"\($0)"}
-                        .joined(separator: ",")
-                    
+                    self.firstNumber = self.magicNumber.randomNumber()
+                    self.secondNumber = self.magicNumber.randomNumber()
+                    self.thirdNumber = self.magicNumber.randomNumber()
+                    self.fourthNumber = self.magicNumber.randomNumber()
+                    self.fifthNumber = self.magicNumber.randomNumber()
                 }) {
                     Text("랜덤 시작!")
                 }
-            }.navigationBarTitle(Text("인생 역전 가즈앗!!!"))
+            }//.navigationBarTitle(Text("인생 역전 가즈앗!!!"))
         }
         
+    }
+}
+
+extension Collection where Element==Int {
+    func randomNumber() -> String {
+        return shuffled().enumerated()
+            .compactMap{ $0 < 6 ? $1 : nil }
+            .sorted(by: <)
+            .map{"\($0)"}
+            .joined(separator: ",")
     }
 }
 
